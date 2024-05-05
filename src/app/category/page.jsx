5b99d3fab70c08e47b607329ae9e133b88cff817
jeetenmndl <jeetenmndl96@gmail.com>
2024-05-05 22:13:51 +0545

    category page component

--- src/app/category/page.jsx
+++ src/app/category/page.jsx
@@ -1,81 +1,12 @@
+import CategoryPage from '@/components/CategoryPage'
 import React from 'react'
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import getAllRooms from '@/lib/actions/getAllRooms'
-import RoomCard from '@/components/RoomCard';
 
 
 const page = async () => {
 
-const rooms = await getAllRooms();
-
-
   return (
     <main className='px-4 md:px-20 py-8'>
-        <Tabs defaultValue="rooms" className="w-full">
-            <div className="flex justify-center sticky  top-4 z-30 ">
-        <TabsList className="py-6 px-2 rounded-md">
-            <TabsTrigger value="rooms" className="py-2 px-6 md:px-8">Rooms</TabsTrigger>
-            <TabsTrigger value="flat" className="py-2 px-6 md:px-8">Flat</TabsTrigger>
-            <TabsTrigger value="home" className="py-2 px-6 md:px-8">Home</TabsTrigger>
-            <TabsTrigger value="office" className="py-2 px-6 md:px-8">Office</TabsTrigger>
-        </TabsList>
-        </div>
-        <TabsContent className="my-8" value="rooms">
-        <article className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-8">
-            {
-                rooms && rooms.map((room)=>{
-                    if(room.rooms.type=="Room"){
-                     return (
-                    <RoomCard key={room._id} room={room} />
-                    )
-                    }
-                })
-            }
-            </article>
-        </TabsContent>
-
-        <TabsContent className="my-8" value="flat">
-        <article className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-8">
-            {
-                rooms && rooms.map((room)=>{
-                    if(room.rooms.type=="Flat"){
-                     return (
-                    <RoomCard key={room._id} room={room} />
-                    )
-                    }
-                })
-            }
-            </article>
-        </TabsContent>
-
-        <TabsContent className="my-8" value="home">
-        <article className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-8">
-            {
-                rooms && rooms.map((room)=>{
-                    if(room.rooms.type=="Home"){
-                     return (
-                    <RoomCard key={room._id} room={room} />
-                    )
-                    }
-                })
-            }
-            </article>
-        </TabsContent>
-
-        <TabsContent className="my-8" value="office">
-        <article className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-8">
-            {
-                rooms && rooms.map((room)=>{
-                    if(room.rooms.type=="Office"){
-                     return (
-                    <RoomCard key={room._id} room={room} />
-                    )
-                    }
-                })
-            }
-            </article>
-        </TabsContent>
-        </Tabs>
+        <CategoryPage />
     </main>
   )
 }
